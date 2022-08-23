d095c0d7e830ca5d2b9a6e1659a1624e5698c506
ashiqsultan <ashiqdrive@gmail.com>
2022-08-23 19:50:21 +0530

    Added styles for Create poll form

--- src/components/CreatePoll.tsx
+++ src/components/CreatePoll.tsx
@@ -11,7 +11,10 @@ import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
-import { createPoll, Poll } from '../api';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { createPoll } from '../api';
 
 export default function CreatePoll() {
   const [pollTitle, setPollTitle] = useState('');
@@ -49,73 +52,110 @@ export default function CreatePoll() {
     // Show Poll url
   };
 
-  return (
-    <div>
-      <div>
-        <TextField
-          id='text-input-poll-title'
-          label='Poll Title'
-          variant='standard'
-          onChange={(event) => setPollTitle(event.target.value)}
-          value={pollTitle}
-        />
-      </div>
-      <div>
-        <TextField
-          id='text-input-poll-option'
-          label='Poll Option'
-          helperText='Hit Enter key to add Poll options'
-          variant='standard'
-          onKeyPress={handleKeyPress}
-        />
-      </div>
-      <div>
-        <Typography variant='caption'>
-          Once the poll is created the poll link will be generated
+  const isPollData = () => pollTitle || options.length > 0;
+
+  const createdPollData = () => {
+    return (
+      <>
+        <Typography gutterBottom variant='h5' component='div'>
+          Poll Data
         </Typography>
-      </div>
-      <div>
-        <Button
-          variant='contained'
-          size='medium'
-          onClick={async () => {
-            await onCreatePoll();
-          }}
-        >
-          Create Poll
-        </Button>
-      </div>
-      <div>
-        <Typography variant='h3'>{pollTitle}</Typography>
-      </div>
-      <div>
-        {pollUrl && (
-          <div>
-            <Typography variant='h5'>
-              Share the below poll link with your friends
-            </Typography>
-            <Link href={pollUrl} target={'_blank'} variant='h5'>
-              {`${window.location.origin}${pollUrl}`}
-            </Link>
-          </div>
-        )}
-      </div>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableBody>
-            {options.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell component='th' scope='row'>
-                  <IconButton onClick={() => onItemClear(index)}>
-                    <CloseOutlinedIcon />
-                  </IconButton>
-                  {item}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+        <Typography gutterBottom variant='body2' component='div'>
+          Title
+        </Typography>
+        <Typography gutterBottom variant='h6' component='div'>
+          {pollTitle}
+        </Typography>
+        <Typography gutterBottom variant='body2' component='div'>
+          Options list
+        </Typography>
+
+        <TableContainer component={Paper}>
+          <Table>
+            <TableBody>
+              {options.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell component='th' scope='row'>
+                    <IconButton onClick={() => onItemClear(index)}>
+                      <CloseOutlinedIcon />
+                    </IconButton>
+                    {item}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    );
+  };
+  return (
+    <div style={{ padding: '20px' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <div style={{ padding: '20px' }}>
+                <Typography variant='h5'>Create New Poll</Typography>
+                <Typography variant='caption'>
+                  Poll link will be generated once saved
+                </Typography>
+                <div style={{ display: 'flex', paddingTop: '20px' }}>
+                  <TextField
+                    id='text-input-poll-title'
+                    label='Poll Title'
+                    variant='standard'
+                    onChange={(event) => setPollTitle(event.target.value)}
+                    value={pollTitle}
+                    style={{ flexGrow: '1' }}
+                  />
+                </div>
+                <div style={{ display: 'flex', paddingTop: '10px' }}>
+                  <TextField
+                    id='text-input-poll-option'
+                    label='Poll Option'
+                    helperText='Hit Enter key to add Poll options to the list'
+                    variant='standard'
+                    onKeyPress={handleKeyPress}
+                    style={{ flexGrow: '1' }}
+                  />
+                </div>
+                <div style={{ display: 'flex', paddingTop: '30px' }}>
+                  <Button
+                    variant='contained'
+                    size='large'
+                    onClick={async () => {
+                      await onCreatePoll();
+                    }}
+                  >
+                    Save
+                  </Button>
+                </div>
+
+                {pollUrl && (
+                  <div style={{ paddingTop: '30px' }}>
+                    <Typography variant='h6'>
+                      Share the poll link with your friends
+                    </Typography>
+                    <Link href={pollUrl} target={'_blank'} variant='h5'>
+                      {`${window.location.origin}${pollUrl}`}
+                    </Link>
+                  </div>
+                )}
+                <div style={{ marginTop: '25px' }}>
+                  {isPollData() ? (
+                    <div>{createdPollData()}</div>
+                  ) : (
+                    <Typography variant='caption'>
+                      Data you enter will appear here
+                    </Typography>
+                  )}
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
