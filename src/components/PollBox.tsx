69a0ca6b91ad0b8a0f66ec58c61e87844e221a7d
ashiqsultan <ashiqdrive@gmail.com>
2022-08-24 12:54:23 +0530

    Fixed redux update for poll chart

--- src/components/PollBox.tsx
+++ src/components/PollBox.tsx
@@ -11,6 +11,7 @@ import { getVote } from '../utils/voteSession';
 const { POLL_UPDATE, UPDATE_ROOM, LEAVE_ALL_ROOM } = constants.SOCKET_EVENTS;
 
 interface State {
+  pollId: string;
   poll: { name: string; options: string[] };
   pollBox: object;
   isVoted: boolean;
@@ -20,7 +21,10 @@ interface Action {
   payload?: any;
 }
 
+const isUserVoted = (pollId: string) => (getVote(pollId) ? true : false);
+
 const initialState: State = {
+  pollId: '',
   poll: { name: '', options: [] },
   pollBox: {},
   isVoted: false,
@@ -29,7 +33,7 @@ const initialState: State = {
 const ACTIONS = {
   POLL_UPDATED: 'poll_updated',
   POLL_BOX_UPDATED: 'poll_box_updated',
-  VOTED: 'voted',
+  FIRST_LOAD: 'first_load',
 };
 
 function reducer(state: State, action: Action): State {
@@ -41,9 +45,18 @@ function reducer(state: State, action: Action): State {
         pollBox: action.payload.pollBox,
       };
     case ACTIONS.POLL_BOX_UPDATED:
-      return { ...state, pollBox: action.payload.pollBox };
-    case ACTIONS.VOTED:
-      return { ...state, isVoted: true };
+      return {
+        ...state,
+        pollBox: action.payload.pollBox,
+        isVoted: isUserVoted(state.pollId),
+      };
+
+    case ACTIONS.FIRST_LOAD:
+      return {
+        ...state,
+        pollId: action.payload.pollId,
+        isVoted: isUserVoted(state.pollId),
+      };
     default:
       console.log('default action');
       return state;
@@ -61,13 +74,14 @@ const PollBox = (props: Props) => {
   const joinRoom = (pollId: string) => {
     socket.emit(UPDATE_ROOM, { pollId });
   };
-  const getVotes = (pollBox: object): Array<number> => {
+  const generateVotesArr = (pollBox: object): Array<number> => {
     if (pollBox && Object.keys(pollBox).length > 0) {
       return Object.values(pollBox);
     }
     return [];
   };
   useEffect(() => {
+    dispatch({ type: ACTIONS.FIRST_LOAD, payload: { pollId } });
     const getPollData = async () => {
       try {
         const pollData = await getPoll(pollId!);
@@ -91,11 +105,11 @@ const PollBox = (props: Props) => {
     }
     socket.on(POLL_UPDATE, (data: any) => {
       if (data.entityId && data.entityId === pollId) {
-        const action = {
+        const pollBoxAction = {
           type: ACTIONS.POLL_BOX_UPDATED,
           payload: { pollBox: data.pollBox },
         };
-        dispatch(action);
+        dispatch(pollBoxAction);
       }
     });
     // unmount
@@ -119,11 +133,11 @@ const PollBox = (props: Props) => {
                   />
                 </div>
               )}
-              {getVote(pollId || '') && (
+              {state.isVoted && (
                 <div>
                   <PollChart
                     options={state.poll.options}
-                    votes={getVotes(state.pollBox)}
+                    votes={generateVotesArr(state.pollBox)}
                   />
                 </div>
               )}
